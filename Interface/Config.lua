ad837b67f8b6f4288f5eca94d6c8ce1a631b982b
Rolex <67190649+haephaistoss@users.noreply.github.com>
2022-02-05 21:36:02 +0100

    Add files via upload

---
+++ Interface/Config.lua
@@ -0,0 +1,28 @@
+
+-- modules:
+LOAD_LOGINSCENE_TOOL = true
+LOAD_REALMLIST_CHANGER = true
+
+-- miscellanous:
+ENABLE_SAVE_PASSWORD = true
+
+ENABLE_WOW_LOGO = false
+WOW_LOGO_POSITION = "TOPRIGHT"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
